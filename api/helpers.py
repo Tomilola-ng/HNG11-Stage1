c60342d3ad5372520683bb199ca5b8e1cc616d8b
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2024-07-01 23:14:17 +0100

    WEATHER_API_KEY

--- api/helpers.py
+++ api/helpers.py
@@ -2,11 +2,11 @@
 import requests
 from dotenv import load_dotenv
 
-WEATER_API_KEY = load_dotenv('WEATER_API_KEY')
+WEATHER_API_KEY = load_dotenv('WEATHER_API_KEY')
 
 def get_location_and_temperature(client_ip: str) -> any:
     """ Get Location and Temperature data """
-    response = requests.get(f'http://api.weatherapi.com/v1/current.json?key={WEATER_API_KEY}&q=${client_ip}')
+    response = requests.get(f'http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q=${client_ip}')
     jsonResponse = response.json()
 
     return jsonResponse.location.city, round(jsonResponse.current.temp_c)
