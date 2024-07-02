544134a1d2485b3ce2e3c95fb50a1f86970d6c1e
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2024-07-02 23:27:13 +0000

    FIX_IP_ADDRESS_ISSUE

--- api/helpers.py
+++ api/helpers.py
@@ -1,18 +1,25 @@
 """ Helping Function """
+import os
+import socket
 import requests
-from dotenv import load_dotenv
 
-WEATHER_API_KEY = load_dotenv('WEATHER_API_KEY')
+from dotenv import load_dotenv
+load_dotenv()
+
+WEATHER_API_KEY = os.environ.get("WEATHER_API_KEY")
 
 def get_my_ip():
     """ Return IP """
-    return requests.get('https://api.ipify.org').text
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    s.connect(('8.8.8.8', 1))  # Using Google DNS server
+    local_ip = s.getsockname()[0]
+    s.close()
+    return local_ip
 
 def get_location_and_temperature(client_ip: str) -> any:
     """ Get Location and Temperature data """
-    print(WEATHER_API_KEY, client_ip)
-
-    response = requests.get(f'http://api.weatherapi.com/v1/current.json?key=269eca5dce734a71972204904240107&q={client_ip}')
+    response = requests.get(f'http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={client_ip}')
     json_response = response.json()
 
     return json_response['location']['region'], round(json_response['current']['temp_c'])
